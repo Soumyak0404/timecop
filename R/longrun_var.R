303c45943d0011f10ace8dab07af5df7f09c4d7f
Soumyak0404 <soumyajyotik1729@gmail.com>
2026-06-21 14:01:51 -0500

    Update longrun_var.R

--- R/longrun_var.R
+++ R/longrun_var.R
@@ -15,24 +15,59 @@ longrun_var <- function(data, d, n, family) {
 
   F_list <- list()
 
+  # estimate marginal probabilities only for the Ordinal case
+  if ("Ordinal" %in% family) {
+    param_hat <- estimate_marginal_params(data, family, d)
+  }
+
   for(i in 2:(n-1)){
+
     xt <- matrix(dat_c[,i], d, 1)
+
     a1 <- xt %*% t(xt)
     a2 <- matrix(dat_c[,i-1], d, 1) %*% t(xt)
     a  <- vec(rbind(a1,a2))
+
     b1 <- matrix(dat_c[,i+1], d, 1) %*% t(xt)
     b2 <- xt %*% t(xt)
     b  <- vec(rbind(b1, b2))
 
-    # for the Gaussian case (see AN_counts)
-    if ("Gaussian" %in% family){
+    # added Ordinal-Ordinal case
+    if ("Ordinal" %in% family){
+
+      rt <- c()
+
+      for (j in seq_along(family)){
+
+        x_raw <- data[j, i-1]
+        p_hat <- param_hat[[j]]
+
+        rt <- c(
+          rt,
+          as.numeric(x_raw == 0) - p_hat[1],
+          as.numeric(x_raw == 1) - p_hat[2]
+        )
+      }
+
+      rt <- matrix(rt, nrow = length(rt), 1)
+
+      F_list[[i-1]] <- rbind(rt,a,b)
+
+      # for the Gaussian case (see AN_counts)
+    } else if ("Gaussian" %in% family){
+
       rt <- c()
       s <- 1
+
       for (j in seq_along(family)){
+
         if (family[[j]] == "Gaussian") {
+
           rt <- c(rt, xt[j], xt[j]^2)
           s <- s + 1
+
         } else {
+
           rt <- c(rt, xt[j])
         }
       }
@@ -42,15 +77,23 @@ longrun_var <- function(data, d, n, family) {
       F_list[[i-1]] <- rbind(rt,a,b)
 
     } else {
+
       F_list[[i-1]] <- rbind(matrix(dat_c[,i-1], d, 1), a, b)
     }
   }
 
   F_mat <- do.call(cbind, F_list)
 
-  longrun <- getLongRunVar(t(F_mat), bandwidth = "nw", kernel = "ba", demeaning = TRUE, check = FALSE)
-  Sigma   <- longrun$Omega
+  longrun <- getLongRunVar(
+    t(F_mat),
+    bandwidth = "nw",
+    kernel = "ba",
+    demeaning = TRUE,
+    check = FALSE
+  )
+
+  Sigma <- longrun$Omega
 
   return(Sigma)
-
 }
+
