ccb5ff1b5c6b66de0111118069c53305fb0343cd
Soumyak0404 <soumyajyotik1729@gmail.com>
2026-06-21 13:50:36 -0500

    Update estimate_marginal_params.R

--- R/estimate_marginal_params.R
+++ R/estimate_marginal_params.R
@@ -11,6 +11,18 @@ estimate_marginal_params <- function(data, family, d) {
   for (i in seq_len(d)) {
     if (family[[i]] %in% c("Bernoulli", "Poisson")) {
       param_hat[[i]] <- mean(data[i, ], na.rm = TRUE)
+    } else if (family[[i]] == "Ordinal") {
+
+      x_i <- data[i, ]
+
+      if (any(!x_i %in% c(0, 1, 2))) {
+        stop("Ordinal data must only contain values 0, 1, and 2.", call. = FALSE)
+      }
+
+      counts <- tabulate(match(x_i, c(0, 1, 2)), nbins = 3)
+
+      param_hat[[i]] <- counts / sum(counts)
+
     } else if (family[[i]] == "Gaussian") {
       param_hat[[i]] <- c(mean(data[i, ], na.rm = TRUE),
                           var(data[i, ],  na.rm = TRUE))
